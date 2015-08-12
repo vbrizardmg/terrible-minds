a6ddad16875118993d86b58ecc15f394a1b1184c
vbrizardmg <vincent@lesmauvaisgarcons.ca>
2015-08-12 17:06:02 -0400

    init project + version mobile

--- js/app.js
+++ js/app.js
@@ -1,3 +1,41 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+
+window.onload=configurer;
+
+function configurer(e){
+	document.getElementsByTagName("body")[0].className += " js";
+	if($('body').hasClass('js')){
+		$(window).on('scroll',gererFixed);
+
+		$('.btplus').on('click',toggleAccordeon); 
+	}
+}
+
+
+
+function gererFixed(e){
+	$hauteur= $(window).scrollTop();
+	$target=$('#comments').offset().top;
+	if($hauteur>=$target){
+		$('#comments').css("background-attachment","scroll");
+	}else{
+		$('#comments').css("background-attachment","fixed");
+	}
+}
+function toggleAccordeon(e){
+	$row=$(this).parent().parent();
+	//console.log($row);
+	$($row).next('.info').toggleClass("active");
+	$(this).toggleClass("rotated");
+	if($($row).next('.info').hasClass("active")){
+		$($row).next('.info').children().addClass("animateIn");
+		$($row).next('.info').children().removeClass("animateOut");
+	}else{
+		$($row).next('.info').children().addClass("animateOut");
+		$($row).next('.info').children().removeClass("animateIn");
+	}
+	
+}
